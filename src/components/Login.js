625347e199c3996dc4e923cc8021fa975e4dc112
GrandPanda <darichey1@gmail.com>
2019-03-27 22:07:14 -0500

    Fix login button styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Modal from 'react-responsive-modal';
 import { ProviderLogin } from '@inrupt/solid-react-components';
 
+import '../style/Login.css'
+
 class Login extends React.Component {
     state = {
         open: false
@@ -18,11 +20,11 @@ class Login extends React.Component {
     render() {
         const { open } = this.state;
         return (
-            <div className="login">
+            <div className="Login">
                 <button onClick={this.onOpenModal}>Login</button>
 
                 <Modal open={open} onClose={this.onCloseModal} focusTrapped>
-                    <ProviderLogin/>
+                    <ProviderLogin callbackUri="http://localhost:3000"/>
                 </Modal>
             </div>
         );
